2a6cafc467076b1142f1c83ec9b321350b93e319
elraffa <federico@wcanvas.com>
2023-04-03 22:21:53 -0300

    modify guest registration form

--- app/Http/Controllers/GuestRegistrationController.php
+++ app/Http/Controllers/GuestRegistrationController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Guest;
+use App\Models\Patient;
+use App\Models\Event;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class GuestRegistrationController extends Controller
 {
@@ -35,21 +40,73 @@ class GuestRegistrationController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'last_name' => 'required',
-            'email' => 'required|unique:guests,email',
-            'phone_number' => 'required',
-        ]);
 
-        $guest = new Guest();
-        $guest->name = $validatedData['name'];
-        $guest->last_name = $validatedData['last_name'];
-        $guest->email = $validatedData['email'];
-        $guest->phone_number = $validatedData['phone_number'];
-        $guest->save();
+        // DB Transaction
+        DB::transaction(function () use ($request) {
+            $validatedData = $request->validate([
+                'name' => 'required',
+                'last_name' => 'required',
+                'email' => 'required|email|unique:patients',
+                'phone_number' => 'required|unique:patients',
+                'street_address' => 'required',
+                'city' => 'required',
+                'state' => 'required',
+                'postal_code' => 'required',
+                'dni' => 'required|unique:patients',
+                'insurance_name' => 'required',
+                'insurance_number' => 'required|unique:patients'
+            ], [
+                'name.required' => 'El nombre es requerido',
+                'last_name.required' => 'El apellido es requerido',
+                'email.required' => 'El email es requerido',
+                'email.email' => 'El email debe ser válido',
+                'email.unique' => 'El email ya está en uso',
+                'phone_number.required' => 'El número de teléfono es requerido',
+                'phone_number.unique' => 'El número de teléfono ya está en uso',
+                'street_address.required' => 'La dirección es requerida',
+                'city.required' => 'La ciudad es requerida',
+                'state.required' => 'El estado es requerido',
+                'postal_code.required' => 'El código postal es requerido',
+                'dni.required' => 'El DNI es requerido',
+                'dni.unique' => 'El DNI ya está en uso',
+                'insurance_name.required' => 'El nombre de la obra social es requerido',
+                'insurance_number.required' => 'El número de la obra social es requerido',
+                'insurance_number.unique' => 'El número de la obra social ya está en uso'
+            ]
+            );
 
-        return redirect('guest/guest-success')->with('success', 'Gracias por registrarte.');
+            // $guest = new Guest();
+            // $guest->name = $validatedData['name'];
+            // $guest->last_name = $validatedData['last_name'];
+            // $guest->email = $validatedData['email'];
+            // $guest->phone_number = $validatedData['phone_number'];
+            // $guest->save();
+
+            $patient = new Patient();
+            $patient->name = $validatedData['name'];
+            $patient->last_name = $validatedData['last_name'];
+            $patient->email = $validatedData['email'];
+            $patient->phone_number = $validatedData['phone_number'];
+            $patient->street_address = $validatedData['street_address'];
+            $patient->city = $validatedData['city'];
+            $patient->state = $validatedData['state'];
+            $patient->postal_code = $validatedData['postal_code'];
+            $patient->dni = $validatedData['dni'];
+            $patient->insurance_name = $validatedData['insurance_name'];
+            $patient->insurance_number = $validatedData['insurance_number'];
+            $patient->save();
+
+            $doctor = Doctor::find($request->input('doctor_id'));
+            $event = new event();
+            $event->event = "Cita de " . $patient->last_name . " con el Dr. " . $doctor->last_name;
+            $event->patient_id = $patient->id;
+            $event->doctor_id = $request->input('doctor_id');
+            $event->start_date = $request->input('start_date');
+            $event->end_date = $request->input('end_date');
+            $event->save();
+        });
+
+        return redirect('guest/guest-register')->with('success', 'Gracias por registrarte.');
     }
 
     /**
--- resources/views/guest/guest-register.blade.php
+++ resources/views/guest/guest-register.blade.php
@@ -1,16 +1,31 @@
+<?php
+
+use App\Models\Doctor;
+use App\Models\Patient;
+
+    $doctors = Doctor::all();
+    $patients = Patient::all();
+
+?>
+
 <x-guest-layout>
+@if (session('success'))
+    <div class="js-success-message w-full text-white p-6 bg-green-700">
+        {{ session('success') }}
+    </div>
+@endif
 <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6">
-                <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Area de Pacientes</h1>
+                <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Solicita un turno</h1>
                 <hr class="mt-2 mb-6">
                     <div class="mt-10 sm:mt-0">
                         <div class="md:grid md:grid-cols-3 md:gap-6">
                             <div class="md:col-span-1">
                             <div class="px-4 sm:px-0">
-                                <h3 class="text-base font-semibold leading-6 text-gray-900">Agregar Paciente</h3>
-                                <p class="mt-1 text-sm text-gray-600">Ingresar informacion de nuevo paciente.</p>
+                                <h3 class="text-base font-semibold leading-6 text-gray-900">Ingresa tus datos</h3>
+                                <p class="mt-1 text-sm text-gray-600">Te pedimos que ingreses tus datos para registrar tu turno.</p>
                             </div>
                             </div>
                             <div class="mt-5 md:col-span-2 md:mt-0">
@@ -18,6 +33,7 @@
                             <form action="{{ route('guest.register.store') }}" method="POST">
                             @csrf
 
+                                <div class="overflow-hidden shadow sm:rounded-md">
                                 <div class="overflow-hidden shadow sm:rounded-md">
                                 <div class="bg-white px-4 py-5 sm:p-6">
                                     <div class="grid grid-cols-6 gap-6">
@@ -53,9 +69,104 @@
                                         @enderror
                                     </div>
 
-                                   
+                                    <div class="col-span-6 sm:col-span-4">
+                                        <label for="dni" class="block text-sm font-medium leading-6 text-gray-900">DNI</label>
+                                        <input type="text" name="dni" id="dni" autocomplete="dni" value="{{ old('dni') }}" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('dni')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="insurance_name" class="block text-sm font-medium leading-6 text-gray-900">Obra Social / Prepaga</label>
+                                        <input type="text" name="insurance_name" id="insurance_name" autocomplete="insurance_name" value="{{ old('insurance_name') }}" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('insurance_name')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="insurance_number" class="block text-sm font-medium leading-6 text-gray-900">Número de Socio OS</label>
+                                        <input type="text" name="insurance_number" id="insurance_number" autocomplete="insurance_number" value="{{ old('insurance_number') }}" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('insurance_number')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6">
+                                        <label for="street_address" class="block text-sm font-medium leading-6 text-gray-900">Dirección</label>
+                                        <input type="textarea" name="street_address" id="street_address" autocomplete="street_address" value="{{ old('street_address') }}" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('street_address')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                                        <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Ciudad</label>
+                                        <input type="text" name="city" id="city" autocomplete="address-level2" value="{{ old('city') }}" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('city')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                        <label for="state" class="block text-sm font-medium leading-6 text-gray-900">Provincia</label>
+                                        <input type="text" name="state" id="state" value="{{ old('state') }}" autocomplete="address-level1" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('state')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                        <label for="postal_code" class="block text-sm font-medium leading-6 text-gray-900">Código Postal</label>
+                                        <input type="text" name="postal_code" id="postal_code" value="{{ old('postal_code') }}" autocomplete="postal_code" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        @error('postal_code')
+                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        @enderror
+                                    </div>
                                     </div>
                                 </div>
+                                <div class="bg-white px-4 py-5 sm:p-6">
+                                            <div class="grid grid-cols-6 gap-6">
+                                                <div class="col-span-6 sm:col-span-3">
+                                                    <label for="doctor" class="block text-sm font-medium leading-6 text-gray-900">Médico</label>
+                                                    <select name="doctor_id" id="doctor_id" class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                    @foreach ($doctors as $doctor)
+                                                        <option value="{{ $doctor->id }}">{{ $doctor->name }} {{ $doctor->last_name }}</option>
+                                                    @endforeach
+                                                    </select>
+                                                </div>
+                                                <div></div>
+
+                                                <div class="col-span-6 sm:col-span-3">
+                                                    <div>
+                                                        <label for="start_date" class="block text-sm font-medium leading-6 text-gray-900">Fecha y hora inicio:</label>
+                                                        <input 
+                                                            type="datetime-local" 
+                                                            id="start_date"
+                                                            name="start_date" 
+                                                            min="{{ now()->format('Y-m-d\TH:i') }}" 
+                                                            hour-range="9-17"
+                                                            class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-span-6 sm:col-span-3">
+
+                                                    <div>
+                                                        <label for="end_date" class="block text-sm font-medium leading-6 text-gray-900">Fecha y Hora de finalización:</label>
+                                                        <input 
+                                                            type="datetime-local" 
+                                                            id="end_date"
+                                                            name="end_date"           
+                                                            hour-range="9-17"
+                                                            class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                    </div>
+
+
+                                                </div>
+                                            </div>
+                                        </div>
                                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
                                     <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Enviar</button>
                                 </div>
